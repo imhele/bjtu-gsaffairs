8d665383caf99037994e269a417e5d1f8f4213b8
高勃 <bobgao@gaobos-MacBook-Pro.local>
2019-03-11 16:58:36 +0800

    delete mis in footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -6,13 +6,6 @@ import { FormattedMessage } from 'umi-plugin-locale';
 export default () => (
   <Layout.Footer className={styles.footer}>
     <div>
-      <Tooltip title={<FormattedMessage id="app.mis" />}>
-        <span>
-          <a href="//mis.bjtu.edu.cn/">MIS</a>
-        </span>
-      </Tooltip>
-    </div>
-    <div>
       <Tooltip title={<FormattedMessage id="app.slogan" />}>
         <span>
           2019 北京交通大学 <Icon type="copyright" /> 研工部
