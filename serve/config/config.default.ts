9bbfbcd7c32a87692915922883a331789a7f0da9
imhele <work@imhele.com>
2019-03-13 19:59:47 +0800

    enable errcode

--- serve/config/config.default.ts
+++ serve/config/config.default.ts
@@ -22,7 +22,7 @@ export default (appInfo: EggAppInfo): PowerPartial<EggAppConfig> & typeof bizCon
   config.middleware = ['errcode', 'auth', 'timerange'];
 
   config.errcode = {
-    enable: false,
+    enable: true,
   };
 
   config.auth = {
