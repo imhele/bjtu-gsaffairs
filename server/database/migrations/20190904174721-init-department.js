786ed1be2fc5eb7ca0ca224232e67874c8ec26a3
imhele <work@imhele.com>
2019-09-04 23:54:57 +0800

    migrate

---
+++ server/database/migrations/20190904174721-init-department.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const {  STRING } = Sequelize;
+    await queryInterface.createTable('Department', {
+      code: {
+        type: STRING(30),
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: STRING(50),
+        allowNull: false,
+      },
+      shortName: {
+        type: STRING(50),
+        allowNull: false,
+      },
+      parent: {
+        type: STRING(30),
+        allowNull: false,
+      },
+      level: {
+        type: INTEGER,
+        allowNull: true,
+        defaultValue: null,
+      },
+      used: {
+        type: TINYINT,
+        allowNull: false,
+      },
+      szdwh: {
+        type: STRING(30),
+        allowNull: false,
+      },
+      affairsUsed: {
+        type: TINYINT,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('Department');
+  },
+};
