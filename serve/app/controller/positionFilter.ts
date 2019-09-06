8448dd31f7fe2911848de69799da5555950759ed
何乐 <work@imhele.com>
2019-09-06 10:03:41 +0800

    Update positionFilter.ts

--- serve/app/controller/positionFilter.ts
+++ serve/app/controller/positionFilter.ts
@@ -56,7 +56,7 @@ export const filtersMap = {
     id: 'semester',
     title: '学年学期',
     type: SimpleFormItemType.Select,
-    selectOptions: ['2018-2019学年 第二学期', '2019-2020学年 第一学期'].map(value => ({ value })),
+    selectOptions: ['2019-2020学年 第一学期'].map(value => ({ value })),
   },
 };
 
