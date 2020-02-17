a30c560a2ba3ea295b0707c41b53a158831f186e
imhele <work@imhele.com>
2020-02-17 13:13:33 +0800

    update

--- serve/app/service/stuapply.ts
+++ serve/app/service/stuapply.ts
@@ -132,7 +132,7 @@ export default class StuapplyService extends Service {
     extraFormatter: (item: any) => any = i => i,
   ) {
     const { model, service } = this.ctx;
-    const positionFilter: WhereOptions<PositionModel> = { types: filter.type };
+    const positionFilter = { types: filter.type } as WhereOptions<Required<PositionModel>>;
     if (!isAdmin) {
       positionFilter.semester = { [Op.or]: await service.position.getSemesters() };
     }
